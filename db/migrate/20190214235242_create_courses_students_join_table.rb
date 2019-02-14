b6306e692a42c5c5ba9f2f59ad3f4e507dbd6bff
Karl Stolley <karl.stolley@gmail.com>
2019-02-14 17:57:23 -0600

    Write migration for the courses-students join table

--- db/migrate/20190214235242_create_courses_students_join_table.rb
+++ db/migrate/20190214235242_create_courses_students_join_table.rb
@@ -1,4 +1,8 @@
 class CreateCoursesStudentsJoinTable < ActiveRecord::Migration[5.2]
   def change
+    create_join_table :courses, :students do |t|
+      t.index :course_id
+      t.index :student_id
+    end
   end
 end
