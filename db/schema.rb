eb9d1d335475dc8c8baf75d87de5fdf7dabf2540
Karl Stolley <karl.stolley@gmail.com>
2019-02-14 17:58:02 -0600

    Run migration to create courses-student join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_234602) do
+ActiveRecord::Schema.define(version: 2019_02_14_235242) do
 
   create_table "courses", force: :cascade do |t|
     t.string "identifier"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2019_02_14_234602) do
     t.string "site_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "courses_students", id: false, force: :cascade do |t|
+    t.integer "course_id", null: false
+    t.integer "student_id", null: false
+    t.index ["course_id"], name: "index_courses_students_on_course_id"
+    t.index ["student_id"], name: "index_courses_students_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|
