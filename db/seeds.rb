c254fa029465bfe9dbeb20869d61e657e47079ac
Karl Stolley <karl.stolley@gmail.com>
2019-02-14 18:36:09 -0600

    Add seed data
    Note that the seed file deletes all existing records (and associations)
    so that it's possible to start again from scratch by running
    
      $ rake db:reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Take the database tables back to zero records
+Course.destroy_all
+Student.destroy_all
+
+@app = Course.create(
+  identifier: 'COM 330',
+  title: 'Web Application Development',
+  term: 'Spring',
+  year: 2019,
+  github_org: 'app-2019',
+  site_url: 'https://courses.karlstolley.com/app/'
+)
+@hci = Course.create(
+  identifier: 'ITMD 362',
+  title: 'Human-Computer Interaction and Web Design',
+  term: 'Spring',
+  year: 2019,
+  github_org: 'itmd-362-2019',
+  site_url: 'https://courses.karlstolley.com/hci/'
+)
+Student.create(
+  [
+    {first_name:"Jenny",last_name:"Webapp",email_address:"jenny@hawk.example.com",github_username:"jenny_github",courses:[@app]},
+    {first_name:"Herman",last_name:"See-Eye",email_address:"herman@hawk.example.com",github_username:"herman_github",courses:[@hci]},
+    {first_name:"Melvin",last_name:"Ambition",email_address:"melvin@hawk.example.com",github_username:"melvin_github",courses:[@app,@hci]}
+  ]
+)
