299a3d28d69cf4b67cb60258362a3c3367159ef3
Karl Stolley <karl.stolley@gmail.com>
2019-02-15 09:31:34 -0600

    Validate year as four digits, now or in the future

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
   has_and_belongs_to_many :students
+  validates :year,
+    numericality: { integer_only: true, greater_than_or_equal_to: Date.today.year },
+    length: { is: 4 }
 end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -4,4 +4,19 @@ class CourseTest < ActiveSupport::TestCase
   test "count students through courses" do
     assert_equal 2, courses(:hci).students.length
   end
+  test "year must be four digits, now or in the future" do
+    current_year = Date.today.year
+    @course = courses(:hci)
+    assert_equal 4, @course.year.to_s.length
+    @course.year = 55555
+    assert_not @course.valid?
+    @course.year = "two-thousand and twenty"
+    assert_not @course.valid?
+    @course.year = current_year - 1
+    assert_not @course.valid?
+    @course.year = current_year
+    assert @course.valid?
+    @course.year = current_year + 1
+    assert @course.valid?
+  end
 end
