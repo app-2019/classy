77e0c36036c6576aaea023ab387d2814a6f731b7
Karl Stolley <karl.stolley@gmail.com>
2019-02-20 17:44:34 -0600

    Set up instance variables in student tests

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,11 +1,17 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
+  setup do
+    # Create instances for more readable, Rails-like tests
+    @melvin = students(:melvin)
+    @jenny = students(:jenny)
+  end
+
   test "count courses through students" do
-    assert_equal 2, students(:melvin).courses.length
-    assert_equal 1, students(:jenny).courses.length
+    assert_equal 2, @melvin.courses.length
+    assert_equal 1, @jenny.courses.length
   end
   test "show full name in custom method" do
-    assert_equal "#{students(:melvin).first_name} #{students(:melvin).last_name}", students(:melvin).full_name
+    assert_equal "#{@melvin.first_name} #{@melvin.last_name}", @melvin.full_name
   end
 end
